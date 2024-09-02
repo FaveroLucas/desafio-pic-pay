a1324710ff9a692661c12d465056a4d1590ae17d
lucas.favero <lucas.favero@accesstage.com.br>
2024-09-02 13:56:38 -0300

    create userService

--- src/main/java/com/picsimplificado/repositories/UserRepository.java
+++ src/main/java/com/picsimplificado/repositories/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByDocument(String document);
-    Optional<User> findById(String id);
+    Optional<User> findUserById(Long id);
 }
---
+++ src/main/java/com/picsimplificado/services/UsarService.java
@@ -0,0 +1,35 @@
+package com.picsimplificado.services;
+
+import com.picsimplificado.domain.user.User;
+import com.picsimplificado.enums.UserType;
+import com.picsimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UsarService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuario do tipo lojista não pode realizar transações");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+}
