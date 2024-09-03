449579bfec7483a7ce2d1949d6de5bc7521e7f4c
lucas.favero <lucas.favero@accesstage.com.br>
2024-09-03 10:35:00 -0300

    treat exceptions

--- src/main/java/com/picsimplificado/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/picsimplificado/exceptions/ControllerExceptionHandler.java
@@ -12,18 +12,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity treatDuplicateEntry(DataIntegrityViolationException exception){
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
         ExceptionDTO exceptionDTO = new ExceptionDTO("Usuario ja cadastrado", "400");
         return ResponseEntity.badRequest().body(exceptionDTO);
     }
 
     @ExceptionHandler(EntityExistsException.class)
-    public ResponseEntity treat404(EntityExistsException exception){
+    public ResponseEntity threat404(EntityExistsException exception){
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity treatGeneralException(EntityNotFoundException exception){
+    public ResponseEntity threatGeneralException(EntityNotFoundException exception){
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
         return ResponseEntity.internalServerError().body(exceptionDTO);
     }
